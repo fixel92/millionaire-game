2b68d7f4f31453661c2a32eac94eb38e1b3eef8e
fixel92 <goodzoon92@gmail.com>
2020-01-14 18:32:40 +0300

    added tests to game_controller at anonym user

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -75,6 +75,19 @@ RSpec.describe GamesController, type: :controller do
         expect(flash[:warning]).to be
       end
     end
+
+    context 'user anonym' do
+      it 'returns user profile and warning' do
+        game_w_questions.update_attribute(:current_level, 2)
+
+        put :take_money, id: game_w_questions.id
+        game = assigns(:game)
+
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(new_user_session_path)
+        expect(flash[:alert]).to be
+      end
+    end
   end
 
   describe '#create' do
@@ -102,7 +115,32 @@ RSpec.describe GamesController, type: :controller do
         expect(response).to redirect_to game_path(game_w_questions)
         expect(flash[:alert]).to be
       end
+    end
 
+    context 'anonym user' do
+      it 'redirect to user auth' do
+        post :create
+        game = assigns(:game)
+
+        expect(game).to be_nil
+        expect(response.status).to eq 302
+        expect(response).to redirect_to(new_user_session_path)
+        expect(flash[:alert]).to be
+      end
+    end
+  end
+
+  describe '#answer' do
+    context 'anonym user' do
+      it 'redirect to user_path' do
+        generate_questions(60)
+        put :answer, id: game_w_questions.id, letter: game_w_questions.current_game_question.correct_answer_key
+        game = assigns(:game)
+
+        expect(game).to be_nil
+        expect(response).to redirect_to(new_user_session_path)
+        expect(flash[:alert]).to be
+      end
     end
   end
 end
