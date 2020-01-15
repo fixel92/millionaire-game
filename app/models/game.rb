34308485960fa442d5c26f83f87559f767855dd8
fixel92 <goodzoon92@gmail.com>
2020-01-15 11:20:06 +0300

    created help user in game

--- app/models/game.rb
+++ app/models/game.rb
@@ -133,6 +133,19 @@ class Game < ActiveRecord::Base
     end
   end
 
+  def use_help(help_type)
+    case help_type
+    when :audience_help
+      unless audience_help_used
+        toggle!(:audience_help_used)
+        current_game_question.add_audience_help
+
+        true
+      end
+      false
+    end
+  end
+
   # Метод take_money! записывает юзеру игровую сумму на счет и завершает игру,
   def take_money!
     # Из законченной или неначатой игры нечего брать
--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -157,4 +157,25 @@ RSpec.describe GamesController, type: :controller do
       end
     end
   end
+
+  describe 'audience help' do
+    context 'auth user' do
+      before(:each) { sign_in user }
+
+      it 'used help and redirect' do
+        expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
+        expect(game_w_questions.audience_help_used).to be_falsey
+
+        put :help, id: game_w_questions.id, help_type: :audience_help
+        game = assigns(:game)
+
+        expect(game.finished?).to be_falsey
+        expect(game.audience_help_used).to be_truthy
+        expect(game.current_game_question.help_hash[:audience_help]).to be
+        expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c' ,'d')
+        expect(response).to redirect_to game_path(game)
+      end
+
+    end
+  end
 end
